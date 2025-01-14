306f8d2916f65da0a4d46c05ea6f03c578e5154b
binhvu3 <binhhtvu@proton.me>
2025-01-14 11:55:39 -0800

    update incoming-call response

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import websockets
 from fastapi import FastAPI, WebSocket, Request
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.websockets import WebSocketDisconnect
-from twilio.twiml.voice_response import VoiceResponse, Connect, Say, Stream
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -15,16 +14,22 @@ load_dotenv()
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 PORT = int(os.getenv('PORT', 5050))
 SYSTEM_MESSAGE = (
-    "You are a helpful and bubbly AI assistant who loves to chat about "
-    "anything the user is interested in and is prepared to offer them facts. "
-    "You have a penchant for dad jokes, owl jokes, and rickrolling – subtly. "
-    "Always stay positive, but work in a joke when appropriate."
-)
+    """
+    You are a helpful and bubbly AI assistant who loves to chat about
+    anything the user is interested in and is prepared to offer them facts.
+    You have a penchant for dad jokes, owl jokes, and rickrolling – subtly. 
+    Always stay positive, but work in a joke when appropriate.
+    """
+    )
 VOICE = 'alloy'
 LOG_EVENT_TYPES = [
-    'error', 'response.content.done', 'rate_limits.updated',
-    'response.done', 'input_audio_buffer.committed',
-    'input_audio_buffer.speech_stopped', 'input_audio_buffer.speech_started',
+    'error', 
+    'response.content.done', 
+    'rate_limits.updated',
+    'response.done', 
+    'input_audio_buffer.committed',
+    'input_audio_buffer.speech_stopped', 
+    'input_audio_buffer.speech_started',
     'session.created'
 ]
 SHOW_TIMING_MATH = False
@@ -36,20 +41,22 @@ if not OPENAI_API_KEY:
 
 @app.get("/", response_class=JSONResponse)
 async def index_page():
-    return {"message": "Twilio Media Stream Server is running!"}
+    return {"message": "Media Stream Server is running!"}
 
 @app.api_route("/incoming-call", methods=["GET", "POST"])
 async def handle_incoming_call(request: Request):
-    """Handle incoming call and return TwiML response to connect to Media Stream."""
-    response = VoiceResponse()
-    # <Say> punctuation to improve text-to-speech flow
-    response.say("Please wait while we connect your call to the A. I. voice assistant, powered by Twilio and the Open-A.I. Realtime API")
-    response.pause(length=1)
-    response.say("O.K. you can start talking!")
+    """Handle incoming call and return media specific XML response to connect to Media Stream."""
     host = request.url.hostname
-    connect = Connect()
-    connect.stream(url=f'wss://{host}/media-stream')
-    response.append(connect)
+    response = f"""
+                <?xml version="1.0" encoding="UTF-8"?>
+                    <Response>
+                        <Say>Hi, you have called Bart's Automative Centre. How can we help?</Say>
+                        <Connect>
+                            <Stream url="wss://{host}/media-stream" />
+                        </Connect>
+                    </Response>
+    """
+    
     return HTMLResponse(content=str(response), media_type="application/xml")
 
 @app.websocket("/media-stream")
